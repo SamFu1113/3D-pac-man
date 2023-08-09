a434432dee569e7badb4c5570a78d5a6f8ffb80a
SamFu1113 <sam86111374@gmail.com>
2023-08-09 20:16:06 +0800

    Add a "C#" file in the "Scripts" directory
    Add "MouseLook.cs" file in the "\Assets\Scripts" directory.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
